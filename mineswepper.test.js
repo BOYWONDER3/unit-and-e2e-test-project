0c4485cb7b075531f6f0778b1ddcd3bc4dac4e77
boywonder3 <opeyemiisaacboywonder@gmail.com>
2023-03-06 10:20:12 +0100

    third commit

--- mineswepper.test.js
+++ mineswepper.test.js
@@ -1,4 +1,5 @@
-import { createBoard, TILE_STATUSES, markedTilesCount, markTile } from "./mineswepper"
+import { describe } from "node:test"
+import { createBoard, TILE_STATUSES, markedTilesCount, markTile, revealTile, checkWin } from "./mineswepper"
 
 describe('#createBoard', () => {
     test('it creates a valid board', () => {
@@ -15,6 +16,7 @@ describe('#createBoard', () => {
         expect(board).toEqual(expectedBoard)
     })
 })
+
 
 describe('#markedTilesCount', () => {
     test('with some tiles marked', () => {
@@ -97,5 +99,131 @@ describe('#markTile', () => {
             { x: 1, y: 1, status: TILE_STATUSES.HIDDEN, mine: false}
         ]]
         expect(markTile(board, { x: 0, y: 0})).toEqual(board)
+    })
+
+    test('with a number tile it does nothing', () => {
+        const board = [[
+            { x: 0, y: 0, status: TILE_STATUSES.NUMBER, mine: false},
+            { x: 0, y: 1, status: TILE_STATUSES.HIDDEN, mine: true}
+        ], [
+            { x: 1, y: 0, status: TILE_STATUSES.HIDDEN, mine: false},
+            { x: 1, y: 1, status: TILE_STATUSES.HIDDEN, mine: false}
+        ]]
+        expect(markTile(board, { x: 0, y: 0})).toEqual(board)
+    })
+})
+
+describe('#revealTile', () => {
+        describe('with a hidden tile', () => {
+            const board = [[
+                { x: 0, y: 0, status: TILE_STATUSES.HIDDEN, mine: false},
+                { x: 0, y: 1, status: TILE_STATUSES.HIDDEN, mine: true}
+            ], [
+                { x: 1, y: 0, status: TILE_STATUSES.HIDDEN, mine: false},
+                { x: 1, y: 1, status: TILE_STATUSES.HIDDEN, mine: false}
+            ],]
+            
+            test('when the tile is a mine it sets its status to  mine', () => {
+                const expectedBoard = [[
+                    { x: 0, y: 0, status: TILE_STATUSES.HIDDEN, mine: false},
+                    { x: 0, y: 1, status: TILE_STATUSES.MINE, mine: true}
+                ], [
+                    { x: 1, y: 0, status: TILE_STATUSES.HIDDEN, mine: false},
+                    { x: 1, y: 1, status: TILE_STATUSES.HIDDEN, mine: false}
+                ],]
+                expect(revealTile(board, { x: 0, y: 1})).toEqual(expectedBoard)
+            })
+
+            describe('when the tile is not a mine', () => {
+                test('when the tile is adjacent to a mine it counts the number of nearby mines', () => {
+                    const expectedBoard = [[
+                        { x: 0, y: 0, status: TILE_STATUSES.NUMBER, mine: false, adjacentMinesCount: 1},
+                        { x: 0, y: 1, status: TILE_STATUSES.HIDDEN, mine: true}
+                    ], [
+                        { x: 1, y: 0, status: TILE_STATUSES.HIDDEN, mine: false},
+                        { x: 1, y: 1, status: TILE_STATUSES.HIDDEN, mine: false}
+                    ],]
+                    expect(revealTile(board, { x: 0, y: 0})).toEqual(expectedBoard)
+                })
+
+                test('when the tile is not adjacent to a mine it reveals nearby tiles', () => {
+                    const board = [[
+                        { x: 0, y: 0, status: TILE_STATUSES.HIDDEN, mine: true},
+                        { x: 0, y: 1, status: TILE_STATUSES.HIDDEN, mine: false},
+                        { x: 0, y: 2, status: TILE_STATUSES.HIDDEN, mine: false},
+                    ],
+                    [
+                        { x: 1, y: 0, status: TILE_STATUSES.HIDDEN, mine: false},
+                        { x: 1, y: 1, status: TILE_STATUSES.HIDDEN, mine: false},
+                        { x: 1, y: 2, status: TILE_STATUSES.HIDDEN, mine: false},
+                    ],
+                    [
+                        { x: 2, y: 0, status: TILE_STATUSES.HIDDEN, mine: false},
+                        { x: 2, y: 1, status: TILE_STATUSES.HIDDEN, mine: false},
+                        { x: 2, y: 2, status: TILE_STATUSES.HIDDEN, mine: false},
+                    ],
+                    ]
+
+                    const expectedBoard = [[
+                        { x: 0, y: 0, status: TILE_STATUSES.HIDDEN, mine: true},
+                        { x: 0, y: 1, status: TILE_STATUSES.NUMBER, mine: false, adjacentMinesCount: 1},
+                        { x: 0, y: 2, status: TILE_STATUSES.NUMBER, mine: false, adjacentMinesCount: 0},
+                    ],
+                    [
+                        { x: 1, y: 0, status: TILE_STATUSES.NUMBER, mine: false, adjacentMinesCount: 1},
+                        { x: 1, y: 1, status: TILE_STATUSES.NUMBER, mine: false, adjacentMinesCount: 1},
+                        { x: 1, y: 2, status: TILE_STATUSES.NUMBER, mine: false, adjacentMinesCount: 0},
+                    ],
+                    [
+                        { x: 2, y: 0, status: TILE_STATUSES.NUMBER, mine: false, adjacentMinesCount: 0},
+                        { x: 2, y: 1, status: TILE_STATUSES.NUMBER, mine: false, adjacentMinesCount: 0},
+                        { x: 2, y: 2, status: TILE_STATUSES.NUMBER, mine: false, adjacentMinesCount: 0},
+                    ],
+                    ]
+
+                    expect(revealTile(board, { x: 2, y: 2})).toEqual(expectedBoard)
+                })
+            })
+        })
+
+    test('with a marked tile it does nothing', () => {
+        const board = [[
+            { x: 0, y: 0, status: TILE_STATUSES.MARKED, mine: false},
+            { x: 0, y: 1, status: TILE_STATUSES.HIDDEN, mine: true}
+        ], [
+            { x: 1, y: 0, status: TILE_STATUSES.HIDDEN, mine: false},
+            { x: 1, y: 1, status: TILE_STATUSES.HIDDEN, mine: false}
+        ]]
+        expect(revealTile(board, { x: 0, y: 0})).toEqual(board)
+    })
+
+    test('with a mine tile it does nothing', () => {
+        const board = [[
+            { x: 0, y: 0, status: TILE_STATUSES.MINE, mine: false},
+            { x: 0, y: 1, status: TILE_STATUSES.HIDDEN, mine: true}
+        ], [
+            { x: 1, y: 0, status: TILE_STATUSES.HIDDEN, mine: false},
+            { x: 1, y: 1, status: TILE_STATUSES.HIDDEN, mine: false}
+        ]]
+        expect(revealTile(board, { x: 0, y: 0})).toEqual(board)
+    })
+
+    test('with a number tile it does nothing', () => {
+        const board = [[
+            { x: 0, y: 0, status: TILE_STATUSES.NUMBER, mine: false},
+            { x: 0, y: 1, status: TILE_STATUSES.HIDDEN, mine: true}
+        ], [
+            { x: 1, y: 0, status: TILE_STATUSES.HIDDEN, mine: false},
+            { x: 1, y: 1, status: TILE_STATUSES.HIDDEN, mine: false}
+        ]]
+        expect(revealTile(board, { x: 0, y: 0})).toEqual(board)
+    })
+})
+
+describe('#checkWin', () => {
+    test('test with only hidden and marked mine tiles it returns true', () => {
+        const board = [[
+            {}
+        ]]
     })
 })
